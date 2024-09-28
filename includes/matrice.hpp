aa5702362df061c588029acd83ea6296f25f1f3e
tmoragli <tmoragli@student.42.fr>
2024-09-28 20:18:27 +0200

    Renamed math.hpp into matrice.hpp

--- includes/matrice.hpp
+++ includes/matrice.hpp
@@ -6,11 +6,12 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 00:32:35 by tmoragli          #+#    #+#             */
-/*   Updated: 2024/09/28 20:12:42 by tmoragli         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:16:50 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include "utils.hpp"
 
 namespace scop {
 	//TODO: matrices
